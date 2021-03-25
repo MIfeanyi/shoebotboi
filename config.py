4c03bef9c62c36c7d17d0249d755deec69d29d4f
Michael Redford <michaeliredford@gmail.com>
2021-03-25 18:51:27 +0800

    it's it

---
+++ config.py
@@ -0,0 +1,21 @@
+
+keys = {
+        "product_url": "https://www.footlocker.sg/en/p/jordan-5-retro-men-shoes-120?v=314100525204#!searchCategory=all",
+        "site_cart": "https://www.footlocker.sg/en/cart",
+        "product_size_btn": "fl-product-size--item",
+        "shoe_size": "11",
+        "fname": "John",
+        "lname": "Smith",
+        "email": "email@gmail.com",
+        "phone_number": "6789998212",
+        "street_address": "Forest Hills Dr.",
+        "city": "Timbuktu",
+        "zip_code": "91601",
+        "card_cvv": "666",
+        "card_number": "1228199669918221"
+}
+
+footlocker = {
+        "footlocker_en": "www.footlocker.com/en/",
+        "footlocker_sg": "www.footlocker.sg/en/"
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+import bot as bt
+
+if __name__ == '__main__':
+    b = bt.Bot()
+    b.order_from_footlocker()
+    
